f890f29520d49f1c0813a10fb72ae0a56b9890ef
Malhar Gohel <ac98824@avcol.school.nz>
2021-06-29 12:24:12 +1200

    Got contact form working

--- WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/Controllers/HomeController.cs
@@ -1,11 +1,16 @@
 ﻿using CURDOperationWithImageUploadCore5_Demo.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication3.Helpers;
 using WebApplication3.Models;
 
 namespace WebApplication3.Controllers
@@ -15,10 +20,15 @@ namespace WebApplication3.Controllers
         public class HomeController : Controller
         {
             private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private IConfiguration configuration;
 
-            public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration _configuration, IWebHostEnvironment hostEnvironment
+            )
             {
+            configuration = _configuration;
                 _logger = logger;
+            webHostEnvironment = hostEnvironment;
             }
 
             public IActionResult Index()
@@ -46,12 +56,45 @@ namespace WebApplication3.Controllers
                 return View();
             }
 
-            public IActionResult Contact()
+        [HttpGet]    
+        public IActionResult Contact()
             {
                 return View();
             }
+        
+        [HttpPost]
+        public ActionResult Contact(ContactToEmail contacttoemail, IFormFile[] attachments)
+        {
+            var body = "Name: " + contacttoemail.Name + "<br>Email: " + contacttoemail.Email + "<br>Message: " + contacttoemail.Message + "<br>";
+            var mailHelper = new MailHelper(configuration);
+            List<string> fileNames = null;
+            if (attachments != null && attachments.Length > 0)
+            {
+                fileNames = new List<string>();
+                foreach (IFormFile attachment in attachments)
+                {
+                    var path = Path.Combine(webHostEnvironment.WebRootPath, "uploads", attachment.FileName);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        attachment.CopyToAsync(stream);
+                    }
+                    fileNames.Add(path);
+                }
+            }
+            if (mailHelper.Send(contacttoemail.Email, configuration["Gmail:Username"], contacttoemail.Subject, body, fileNames))
+            {
+                ViewBag.msg = "Sent Mail Successfully";
+            }
+            else
+            {
+                ViewBag.msg = "Failed";
+            }
+            return View("Sent", new ContactToEmail());
 
-            public IActionResult Shop()
+
+        }
+
+        public IActionResult Shop()
             {
                 return View();
             }
---
+++ WebApplication3/Models/ContactToEmail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class ContactToEmail
+    {
+        [Required, Display(Name = "Your name")]
+        public string Name { get; set; }
+        [Required, Display(Name = "Your email"), EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
+        //public HttpPostedFileBase Upload { get; set; }
+
+        
+        [Required]
+        public string Subject { get; set; }
+
+    }
+}
