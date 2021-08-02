3da966a5e667f21f4b06df5e184eb26b6bea6932
Malhar Gohel <ac98824@avcol.school.nz>
2021-08-02 14:14:49 +1200

    Added Admin Roles

---
+++ WebApplication3/ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        [Display(Name = "Role")]
+        public string RoleName { get; set; }
+    }
+}
