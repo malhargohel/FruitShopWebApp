c6ba38244d0d699fa7b3f7fef5543a6537454c03
Malhar Gohel <ac98824@avcol.school.nz>
2021-08-11 10:41:56 +1200

    Fixed Loader Problem

--- WebApplication3/wwwroot/js/main.js
+++ WebApplication3/wwwroot/js/main.js
@@ -49,15 +49,15 @@
 	};
 	fullHeight();
 
-	//// loader
-	//var loader = function() {
-	//	setTimeout(function() { 
-	//		if($('#ftco-loader').length > 0) {
-	//			$('#ftco-loader').removeClass('show');
-	//		}
-	//	}, 1);
-	//};
-	//loader();
+	// loader
+	var loader = function() {
+		setTimeout(function() { 
+			if($('#ftco-loader').length > 0) {
+				$('#ftco-loader').removeClass('show');
+			}
+		}, 1);
+	};
+	loader();
 
 	// Scrollax
    $.Scrollax();
