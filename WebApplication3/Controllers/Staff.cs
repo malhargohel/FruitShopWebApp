d8c829965694cfbfdf4e151d555871b9da1ec128
Malhar Gohel <ac98824@avcol.school.nz>
2021-09-19 21:53:53 +1200

    New Table

---
+++ WebApplication3/Controllers/Staff.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Controllers
+{
+    public class Staff : Controller
+    {
+        https://solarmovie.pe/watch-movie/watch-the-dark-knight-free-19752.2018414
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
